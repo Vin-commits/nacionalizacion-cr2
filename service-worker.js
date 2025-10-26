0d83b4391a52ac8572a2126ed76091310b662ece
Vin-commits <edwin.valverdeg@gmail.com>
2025-10-26 00:39:45 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Nombre de la caché
-const CACHE_NAME = 'nacionalizacion-cr-cache-v23'; // CAMBIO V4
+const CACHE_NAME = 'nacionalizacion-cr-cache-v24'; // CAMBIO V4
 
 // Archivos para guardar en caché (el "App Shell")
 const CACHE_FILES = [
